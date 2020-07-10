2bd237d11fe55b8514d8fcc226e20e183c6c96df
saulogp <sauloproetti@gmail.com>
2020-07-10 13:16:21 -0300

    criação do appbar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,7 +3,23 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          height: 35,
+          child: Image.asset("images/logo-yt.png"),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.black,
+        actions: <Widget>[
+          Align(
+            alignment: Alignment.center,
+            child: Text("0"),
+          ),
+          IconButton(icon: Icon(Icons.star), onPressed: (){}),
+          IconButton(icon: Icon(Icons.search), onPressed: (){}),
+        ],
+      ),
       
     );
   }
